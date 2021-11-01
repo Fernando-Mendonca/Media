9942502003cb9ecb1555249627f7983e1c88b0bb
Fernando José Gonçalves Mendonça <nando.jogomend@gmail.com>
2021-11-01 09:28:35 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio Media/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Desafio_Media
+{
+    partial class frmCalcularMedia
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.lblNota1 = new System.Windows.Forms.Label();
+            this.lblNota2 = new System.Windows.Forms.Label();
+            this.lblNota3 = new System.Windows.Forms.Label();
+            this.lblMed = new System.Windows.Forms.Label();
+            this.lblSit = new System.Windows.Forms.Label();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.lblSituacao = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(124, 35);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 20);
+            this.txtNota1.TabIndex = 0;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(124, 101);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 20);
+            this.txtNota2.TabIndex = 1;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(124, 168);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 20);
+            this.txtNota3.TabIndex = 2;
+            // 
+            // lblNota1
+            // 
+            this.lblNota1.AutoSize = true;
+            this.lblNota1.Location = new System.Drawing.Point(50, 38);
+            this.lblNota1.Name = "lblNota1";
+            this.lblNota1.Size = new System.Drawing.Size(36, 13);
+            this.lblNota1.TabIndex = 3;
+            this.lblNota1.Text = "Nota1";
+            // 
+            // lblNota2
+            // 
+            this.lblNota2.AutoSize = true;
+            this.lblNota2.Location = new System.Drawing.Point(53, 104);
+            this.lblNota2.Name = "lblNota2";
+            this.lblNota2.Size = new System.Drawing.Size(36, 13);
+            this.lblNota2.TabIndex = 4;
+            this.lblNota2.Text = "Nota2";
+            // 
+            // lblNota3
+            // 
+            this.lblNota3.AutoSize = true;
+            this.lblNota3.Location = new System.Drawing.Point(53, 168);
+            this.lblNota3.Name = "lblNota3";
+            this.lblNota3.Size = new System.Drawing.Size(36, 13);
+            this.lblNota3.TabIndex = 5;
+            this.lblNota3.Text = "Nota3";
+            // 
+            // lblMed
+            // 
+            this.lblMed.AutoSize = true;
+            this.lblMed.Location = new System.Drawing.Point(53, 232);
+            this.lblMed.Name = "lblMed";
+            this.lblMed.Size = new System.Drawing.Size(48, 13);
+            this.lblMed.TabIndex = 6;
+            this.lblMed.Text = "Media = ";
+            // 
+            // lblSit
+            // 
+            this.lblSit.AutoSize = true;
+            this.lblSit.Location = new System.Drawing.Point(53, 269);
+            this.lblSit.Name = "lblSit";
+            this.lblSit.Size = new System.Drawing.Size(61, 13);
+            this.lblSit.TabIndex = 7;
+            this.lblSit.Text = "Situacao = ";
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Location = new System.Drawing.Point(124, 231);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(13, 13);
+            this.lblMedia.TabIndex = 8;
+            this.lblMedia.Text = "0";
+            // 
+            // lblSituacao
+            // 
+            this.lblSituacao.AutoSize = true;
+            this.lblSituacao.Location = new System.Drawing.Point(127, 269);
+            this.lblSituacao.Name = "lblSituacao";
+            this.lblSituacao.Size = new System.Drawing.Size(0, 13);
+            this.lblSituacao.TabIndex = 9;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(130, 319);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // frmCalcularMedia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblSituacao);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.lblSit);
+            this.Controls.Add(this.lblMed);
+            this.Controls.Add(this.lblNota3);
+            this.Controls.Add(this.lblNota2);
+            this.Controls.Add(this.lblNota1);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Name = "frmCalcularMedia";
+            this.Text = "Calcular Media";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Label lblNota1;
+        private System.Windows.Forms.Label lblNota2;
+        private System.Windows.Forms.Label lblNota3;
+        private System.Windows.Forms.Label lblMed;
+        private System.Windows.Forms.Label lblSit;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Label lblSituacao;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Desafio Media/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio_Media
+{
+    public partial class frmCalcularMedia : Form
+    {
+        public frmCalcularMedia()
+        {
+            InitializeComponent();
+        }
+
+        double Nota1, Nota2, Nota3, Media;
+
+        
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+            Nota1 = Convert.ToDouble(txtNota1.Text);
+            Nota2 = Convert.ToDouble(txtNota2.Text);
+            Nota3 = Convert.ToDouble(txtNota3.Text);
+
+            Media = (Nota1 + Nota2 + Nota3) / 3;
+            lblMedia.Text = Media.ToString();
+                       
+            if (Media < 7)
+            {
+                lblSituacao.Text = ("REPROVADO" );
+            } else
+            {
+                lblSituacao.Text = ("APROVADO" );
+            }                       
+        }
+
+        
+        
+    }
+}
